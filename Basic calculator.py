306c9f4c5e99e90196fc996648224919bc9c3ebb
Your Name <you@example.com>
2024-05-05 16:08:50 +0300

    Updates: Added @staticmethod and functools

--- Basic calculator.py
+++ Basic calculator.py
@@ -1,36 +1,26 @@
-def addition(*numbers):
-    result = 0
-    for num in numbers:
-        result += num
-    return result
+from functools import reduce
 
 
-def subtraction(*numbers):
-    result = numbers[0]
-    for num in numbers[1:]:
-        result -= num
-    return result
+class Calculator:
+
+    @staticmethod
+    def add(*args):
+        return sum(args)
+
+    @staticmethod
+    def multiply(*args):
+        return reduce(lambda x, y: x * y, args)
+
+    @staticmethod
+    def divide(*args):
+        return reduce(lambda x, y: x / y, args)
+
+    @staticmethod
+    def sbtackt(*args):
+        return reduce(lambda x, y: x - y, args)
 
 
-def multiplication(*numbers):
-    result = 1
-    for i in numbers:
-        result *= i
-    return result
-
-
-def division(*numbers):
-    result = numbers[0]
-    for num in numbers[1:]:
-        result /= num
-    return result
-
-
-def factorial(n):
-    if n == 1 or n == 0:
-        return f"Please enter a number other than 1 or 0. \n Your choice is: {n}!"
-    else:
-        result = 1
-    for number in range(n, 1, -1):
-        result *= number
-    return result
+print(Calculator.add(5, 10, 4))
+print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.sbtackt(90, 20, -50, 43, 7))
